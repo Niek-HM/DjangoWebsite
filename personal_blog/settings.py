2f5d42a8877ff017cc2e2c1c40671079513e34eb
Niek-HM <56038147+Niek-HM@users.noreply.github.com>
2021-12-09 19:18:25 +0100

    Update settings.py

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/static')
 MEDIA_URL = '/static/media/'
 
 STATICFILES_DIRS = [
